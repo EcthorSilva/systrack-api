0cee054513dc30d8050840dc4bc5d67debc7da93
Ecthor Silva <EcthorSilva@outlook.com>
2024-06-10 18:03:55 +0000

    add: requisição para pegar as informações da memoria RAM (#1)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
+
+/* Add routes */
 const diskRoutes = require('./diskRoutes');
+const memoryRoutes = require('./memoryRoutes');
 
 router.use('/disk', diskRoutes);
+router.use('/memory', memoryRoutes);
 
 module.exports = router;
