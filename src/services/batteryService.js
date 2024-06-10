10e16df1390df21816e6ddc6d106f13f4edab9d8
Ecthor Silva <EcthorSilva@outlook.com>
2024-06-10 17:26:36 -0300

    fix: corrigindo o erro na hora de chamar algumas funções dentro de batteryInfo

--- src/services/batteryService.js
+++ src/services/batteryService.js
@@ -5,14 +5,14 @@ async function getBatteryInfo() {
       const battery = await si.battery();
 
       const batteryInfo = {
-         hasBattery: battery.hasbattery, // indicates presence of battery
-         cycleCount: battery.cyclecount, // numbers of recharges
-         isCharging: battery.ischarging, // indicates if battery is charging
-         maxCapacity: battery.maxcapacity, // 	max capacity of battery (mWh)
-         currentCapacity: battery.currentcapacity, //	current capacity of battery (mWh)
+         hasBattery: battery.hasBattery, // indicates presence of battery
+         cycleCount: battery.cycleCount, // numbers of recharges
+         isCharging: battery.isCharging, // indicates if battery is charging
+         maxCapacity: battery.maxCapacity, // 	max capacity of battery (mWh)
+         currentCapacity: battery.currentCapacity, //	current capacity of battery (mWh)
          designedCapacity: battery.designedCapacity, // designed capacity of battery (mWh)
          percent: battery.percent, // remaining energy in percentage
-         acConnected: battery.acConnected, // indicates if AC is connected
+         acConnected: battery.acConnected, // indicates if AC is connected 
          health: calculateBatteryHealth(battery)
       };
 
