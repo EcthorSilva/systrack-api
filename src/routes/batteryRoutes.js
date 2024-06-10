b0a2be76b636bdf0ada43a9b6c4c8c431cadb160
Ecthor Silva <EcthorSilva@outlook.com>
2024-06-10 17:24:45 -0300

    Revert "add: Battery (#2)"
    This reverts commit d329ca163904a2a38a5f378bef1c54eecba2ab19.

--- src/routes/batteryRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const batteryController = require('../controllers/batteryController');
-
-const router = express.Router();
-
-router.get('/', batteryController.getBattery);
-
-module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,8 @@ const router = express.Router();
 /* Add routes */
 const diskRoutes = require('./diskRoutes');
 const memoryRoutes = require('./memoryRoutes');
-const batteryRoutes = require('./batteryRoutes');
 
 router.use('/disk', diskRoutes);
 router.use('/memory', memoryRoutes);
-router.use('/battery', batteryRoutes);
 
 module.exports = router;
